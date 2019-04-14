5c6c80d24f9a82b50533c02245f595e866807e9e
SHELLEYJ32 <jiang25s@mtholyoke.edu>
2019-04-14 16:33:40 -0400

    menu transitions

---
+++ PlsTurnOffTheLights/Assets/Scripts/General/ButtonController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonController : MonoBehaviour
+{
+    public void LoadInstruction()
+    {
+        Debug.Log("clicked I");
+        SceneManager.LoadScene("InstructionScene");
+    }
+
+    public void LoadTwitchSetting()
+    {
+        Debug.Log("clicked T");
+        SceneManager.LoadScene("TwitchInputScene");
+    }
+
+    public void Submit()
+    {
+
+    }
+
+    public void LoadOption()
+    {
+        SceneManager.LoadScene("OptionScene");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void LoadFirstLv()
+    {
+        SceneManager.LoadScene("FirstLvScene");
+    }
+
+    public void Pause()
+    {
+
+    }
+
+    public void Resume() { }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
